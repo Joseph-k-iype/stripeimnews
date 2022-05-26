740753d23524fe3405cbc2b871084f06adf98c1d
Joseph K Iype <40719207+Joseph-k-iype@users.noreply.github.com>
2022-05-26 12:48:10 +0530

    commit

--- subscriptions/forms.py
+++ subscriptions/forms.py
@@ -2,19 +2,61 @@
 from django import forms
 from ckeditor.widgets import CKEditorWidget
 from .models import *
-
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+from crispy_forms.bootstrap import FormActions
+from django.template.context_processors import csrf
+from crispy_forms.utils import render_crispy_form
 
 class submitform(forms.ModelForm):
     #form for a particular user
+    headline = forms.CharField(
+            widget=forms.widgets.Textarea(
+                attrs={'style': 'height:10px',
+                    'class':'form-control mb-5'
+                }
+            )
+        )
+    summary = forms.CharField(
+            widget=forms.widgets.Textarea(
+                attrs={'style': 'height:10px',
+                    'class':'form-control mb-5'
+                }
+            )
+        )
+    article = forms.CharField(
+        widget=forms.widgets.Textarea(
+                attrs={
+                    'class':'form-control area_1'
+                }
+            )
+
+    )
+    schedule = forms.DateTimeField(
+        widget=forms.widgets.DateTimeInput(
+            format='%Y-%m-%d %H:%M:%S',
+            attrs={'type': 'datetime-local',
+                'class':'form-control date'
+            }
+        )
+    )
+  
     class Meta:
         model = formforsubmit
-        fields = ['headline', 'summary', 'article', 'schedule', 'prev_img']
+        fields = ['headline', 'summary', 'article', 'schedule']
+        
+
         widgets = {
             'article': CKEditorWidget(),
             'schedule': forms.DateTimeInput(
                 format='%Y-%m-%d %H:%M:%S',
-                attrs={'type': 'datetime-local'}
-            )
+                attrs={'type': 'datetime-local',
+                'class':'form-control time'
+                },
+                
+            ),
+            
+            
         }
 
 #form for sending a mail to the user
